0767bf5070c405df98f7fbf1a6eea962c6a58043
Fran <frmelchor@alumnos.unex.es>
2022-05-26 12:05:48 +0200

    resolve bug in run_project

--- run_project.sh
+++ run_project.sh
@@ -1,4 +1,4 @@
-dvc remote modify mystorage --local gdrive_service_account_json_file_path "$(pwd)"/education-drop-d884963a3b3b.json
+dvc remote modify myremote --local gdrive_service_account_json_file_path "$(pwd)"/education-drop-d884963a3b3b.json
 dvc pull
 PYTHONPATH="$(pwd)"/Code
 export PYTHONPATH
