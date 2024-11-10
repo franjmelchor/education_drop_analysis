6c1795ad2a70d66f2eb8103427c5e4d3becb36cd
Fran <frmelchor@alumnos.unex.es>
2024-11-10 16:21:48 +0100

    update project

--- Code/analysis_and_modeling/6-ROCK/rock_fsel.R
+++ Code/analysis_and_modeling/6-ROCK/rock_fsel.R
@@ -2,12 +2,16 @@ if(!require('cba')) {
   install.packages('cba')
   library('cba')
 }
+library('cba')
+library('fastDummies')
+
 
 analys_personal_data_model_filename<-'./analys_personal_data_model_fsel.csv'
 analys_personal_data_model <- read.csv(file=analys_personal_data_model_filename, 
                                        header = TRUE, stringsAsFactors = TRUE)
 x  = subset(analys_personal_data_model, select = -abandona)
-x <- as.dummy(x)
+x <- dummy_cols(x)
+x <- data.matrix(x)
 rc <-rockCluster(x, n=2, theta=0.13)
 print(rc)
 rf <- fitted(rc)
